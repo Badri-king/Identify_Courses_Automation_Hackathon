aa38d239cbb333ed2e17351afe7a0072194cf58f
V <2497842@cognizant.com>
2026-07-07 22:45:49 +0530

    checking for enterprise feature in jenkins

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/02_LanguageLearning.feature",
+        features = "src/test/resources/features/03_EnterpriseForm.feature",
         glue = {"stepdefinitions", "hooks"},   // must match package names
         monochrome = true,
         plugin = {"pretty",
